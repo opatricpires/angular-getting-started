b093f7fed4ac493b51872ee6cbfbc44020e57d30
opatricpires <patric.pires@outlook.com>
2023-01-04 14:49:35 -0300

    feat: handling input with two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { AppComponent } from "./app.component";
 import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
 import { ProductListComponent } from "./products/product-list.component";
 
 @NgModule({
   declarations: [AppComponent, ProductListComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
