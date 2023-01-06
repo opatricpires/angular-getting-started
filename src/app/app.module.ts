7836214a73eb5f04560bd8b2edfb8c3ff57b232c
opatricpires <patric.pires@outlook.com>
2023-01-06 19:47:15 -0300

    feat: create and using a custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { AppComponent } from "./app.component";
 import { BrowserModule } from "@angular/platform-browser";
+import { ConvertToSpacesPipe } from "./shared/convert-to-space.pipe";
 import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
 import { ProductListComponent } from "./products/product-list.component";
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe],
   imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent],
 })
