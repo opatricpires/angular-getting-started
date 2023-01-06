8a92868d40ecf18df889851c4b9ba441b81ab282
opatricpires <patric.pires@outlook.com>
2023-01-06 19:32:52 -0300

    feat: add custom style and use hooks

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,11 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
 import { IProduct } from "./product";
 
 @Component({
   selector: "pm-products",
   templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   pageTitle = "Product List";
   filterLabel = "Filter by:";
   imageWidth = 50;
@@ -66,5 +68,8 @@ export class ProductListComponent {
   ];
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+  ngOnInit(): void {
+    console.log("In onInit");
   }
 }
